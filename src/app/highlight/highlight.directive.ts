c88199e2a0be84eecdcb011f2e00bc208647439c
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-06-27 19:15:34 -0400

    Binding property using HostBinding

--- src/app/highlight/highlight.directive.ts
+++ src/app/highlight/highlight.directive.ts
@@ -1,14 +1,29 @@
-import { Directive, ElementRef, OnInit, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  OnInit,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
 // add directive
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   // inject a reference to the DOM element
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit() {
     this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.backgroundColor = 'lightBlue';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.backgroundColor = 'transparent';
   }
 }
